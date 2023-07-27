95bdc168c941af07066e03991aed81986228771e
1768SAIKUMAR <chintalasaikumar1768.1@gmail.com>
2023-07-27 22:06:05 +0530

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,30 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Searchbar from "./Searchbar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+
+
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    Searchbar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+
+ 
+    
+    
+}
